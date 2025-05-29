9bad5ba85929fad1be086cee785152ed6772f5cb
Mylena Ribeiro <ribeiro.mylena1@gmail.com>
2025-05-29 19:23:17 -0300

    Fazendo validações no código

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@ let amigos = [];
 
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
+    if(amigo.value === '') {
+        alert('Por favor, digite o nome do amigo.');
+        return;
+    };
+
+    // Verifica se o nome já está na lista:
+     if(amigos.includes(amigo.value)) {
+        alert('Nome já adicionado.');
+        return;
+    };
+
     let listaDeAmigos = document.getElementById('lista-amigos');
     amigos.push(amigo.value);
 
@@ -9,12 +20,17 @@ function adicionar() {
         listaDeAmigos.textContent = amigo.value;
     } else {
         listaDeAmigos.textContent = listaDeAmigos.textContent + ', ' + amigo.value;
-    }
+    };
 
     amigo.value = '';   // Limpa o campo de entrada
 };
 
 function sortear() {
+    if(amigos.length < 4) {
+        alert('É necessário pelo menos 4 amigos para realizar o sorteio.');
+        return;
+    };
+
     embaralha(amigos);
     let sorteio = document.getElementById('lista-sorteio');
 
@@ -23,8 +39,8 @@ function sortear() {
             sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
         } else {
             sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
-        }
-    }
+        };
+    };
 };
 
 function excluirAmigo(index) {
